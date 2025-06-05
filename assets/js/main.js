e16533f69bf2fccc0577d01aa020f1213a0e726a
meike <mart0143@hz.nl>
2025-06-05 09:22:40 +0200

    Basic implementation to gather list of values

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@ import { getList, getOne, getListBasedOnPaginationPage } from './api.js';
 // global variables
 
 // api root
-let apiRoot = 'https://swapi.py4e.com/api/';
-let selectedCollection = 'people';
+let apiRoot = 'https://www.fruityvice.com/api/';
+let selectedCollection = 'fruit';
 
 /**
  * Function to initialize the application
@@ -20,15 +20,33 @@ async function init() {
  * Function to handle the request from the API
  */
 async function handleRequest() {
-  // Logic to handle the request
-  update();
+  // const fruit = await getOne('random',selectedCollection, apiRoot);
+  const fruits = await getList(selectedCollection, apiRoot);
+  // console.log(fruit);
+  console.log(fruits);
+  update(fruits);
 }
 
 /**
  * Function to update the DOM
  */
-function update() {
-  // Logic to update the DOM
+function update(data) {
+  const breweries = data.data; // Assuming the data is returned as an array with a single object
+  const breweriesList = document.getElementById('fruits');
+  breweriesList.innerHTML = ''; // Clear any existing content
+
+  // Create a new div to display the brewery details
+  if (Array.isArray(fruits)) {
+    breweries.forEach(brewery => {
+      const fruitDiv = document.createElement('div');
+      fruitDiv.className = 'bg-[#D9EDC0] w-[90] lg:w-[70%] h-14 p-3 pl-7 font-bold text-lg';
+      fruitDiv.innerHTML = `
+          <h2>${fruit.name}</h2>`;
+      breweriesList.appendChild(fruitDiv);
+    });
+  } else {
+    console.error('Expected an array of fruits, got none');
+  }
 }
 
 init();
