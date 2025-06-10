241acab8d6791a8aa7c85c8e4bd970560b1f8de6
meike <mart0143@hz.nl>
2025-06-10 11:24:08 +0200

    Removed api requests & started writing new about page content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-import { getBreweryTypes, getOne, getListBasedOnPaginationPage } from './api.js';
+import { getOne, getListBasedOnPaginationPage } from './api.js';
 
 // dom elements
 // global variables
@@ -6,7 +6,6 @@ import { getBreweryTypes, getOne, getListBasedOnPaginationPage } from './api.js'
 // api root
 let apiRoot = 'https://api.openbrewerydb.org/v1/';
 let selectedCollection = 'breweries';
-let chosenType = 'micro'
 
 /**
  * Displays an error message on the page
@@ -34,12 +33,6 @@ async function handleBreweryRequest(breweryId) {
   updateBrewery(brewery);
 }
 
-async function handleBreweryTypeRequest() {
-  const breweryType = await getBreweryTypes(selectedCollection, apiRoot, chosenType);
-  console.log(breweryType);
-  updateBreweryTypes(breweryType);
-}
-
 /**
  * Function to display the list of breweries by updating the DOM
  */
@@ -65,37 +58,6 @@ function updateBreweryList(data) {
         </div>
         </section>`;
       breweriesList.appendChild(breweryDiv);
-    });
-  } else {
-    displayError('Expected an array of breweries, got none');
-  }
-}
-
-/**
- * Function to display the list of breweries by updating the DOM
- */
-function updateBreweryTypes(data) {
-  const breweryTypes = data.data; // Assuming the data is returned as an array with a single object
-  const breweryTypeList = document.getElementById('breweryTypes');
-  breweryTypeList.innerHTML = ''; // Clear any existing content
-
-  // Create a new div to display the brewery details
-  if (Array.isArray(breweryTypes)) {
-    breweryTypes.forEach(breweryType => {
-      const breweryTypeDiv = document.createElement('section');
-      breweryTypeDiv.className = 'bg-[#FFC567] h-auto w-[90%] text-left p-2 m-3 font-bold text-xs';
-      breweryTypeDiv.innerHTML = 
-      `<section class="flex flex-row">
-        <div class="basis-7/12">
-          <h2 class=" text-xs lg:text-3xl font-bold m-2 lg:m-0">${breweryType.name}</h2>
-        </div>
-        <div class="basis-5/12">
-          <button class="bg-[#FF9D00] rounded-2xl w-32 h-6 p-2 lg:m-4 flex items-center justify-center">
-            <a class="text-center text-xs font-bold text-white" href="details.html?breweryId=${breweryType.id}">More Information</a>
-          </button>
-        </div>
-        </section>`;
-      breweryTypeList.appendChild(breweryTypeDiv);
     });
   } else {
     displayError('Expected an array of breweries, got none');
@@ -134,11 +96,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             const breweryId = urlParams.get('breweryId');
             console.log(breweryId)
             handleBreweryRequest(breweryId);
-        } else if (path.includes('breweryTypes.html')) {
-            const breweryType = urlParams.get('type');
-            console.log(breweryType)
-            handleBreweryTypeRequest(breweryType);
-        }
+        } 
     } catch (error) {
       displayError(error);
     }
