9a73987477909eaaf21774ba8ff247c5e47a877e
meike <mart0143@hz.nl>
2025-06-12 10:53:21 +0200

    more testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,js}", "!node_modules/**", "./dist/**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "!node_modules/**"],
   theme: {
     extend: {},
   },
